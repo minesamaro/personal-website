1a54e5e7ccecb2bcb80d2d2f30c0fa7e114b5448
minesamaro <22.maria.amaro@gmail.com>
2026-01-17 00:06:26 +1000

    Website revamp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,10 @@
 "use client";
-import Image from "next/image";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import AboutMe from "../components/AboutMe";
 import Skills from "../components/Skills";
-import Certifications from "../components/Certifications";
-import Experience from "../components/Experience";
-import Education from "../components/Education";
 import Contact from "../components/Contact";
-import Languages from "../components/Languages";
 import WhereAmI from "../components/WhereAmI";
-import { motion } from "framer-motion";
 
 
 export default function Home() {
